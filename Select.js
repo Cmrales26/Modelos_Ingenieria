fc9ebf0d7590c20ed8b3b3d64f809183c4465950
Vidal <diana-vidal-10@hotmail.com>
2023-06-13 12:49:35 -0500

    Alerts and some definitions

---
+++ Select.js
@@ -0,0 +1,34 @@
+function Seleccion() {
+
+  const swalWithBootstrapButtons = Swal.mixin({
+    customClass: {
+      confirmButton: 'btn btn-success',
+      cancelButton: 'btn btn-danger'
+    },
+    buttonsStyling: true
+  })
+  
+  swalWithBootstrapButtons.fire({
+    title: '¿Qué quieres hacer primero?',
+    icon: 'question',
+    showCancelButton: true,
+    confirmButtonText: 'Ver simuladores',
+    cancelButtonText: 'Ver tutorial',
+    width: 600,
+    padding: '3em',
+    color: '#716add',
+  }).then((result) => {
+    if (result.isConfirmed) {
+      swalWithBootstrapButtons.fire(
+        window.location.href = "../Cinematica/cinematica.html",
+      )
+    } else if (
+      /* Read more about handling dismissals below */
+      result.dismiss === Swal.DismissReason.cancel
+    ) {
+      swalWithBootstrapButtons.fire(
+        window.location.href = "../ComoUsar/ComoUsarCinematica.html"
+      )
+    }
+  })
+}

ead3ccd50d038b9b9ff9ba5fe1f3773c001437d9
Vidal <diana-vidal-10@hotmail.com>
2023-06-14 09:07:31 -0500

    Add arreglito

--- Select.js
+++ Select.js
@@ -1,4 +1,4 @@
-function Seleccion() {
+function SeleccionCinema() {
 
   const swalWithBootstrapButtons = Swal.mixin({
     customClass: {
@@ -28,6 +28,40 @@ function Seleccion() {
     ) {
       swalWithBootstrapButtons.fire(
         window.location.href = "../ComoUsar/ComoUsarCinematica.html"
+      )
+    }
+  })
+}
+function SeleccionNewton() {
+
+  const swalWithBootstrapButtons = Swal.mixin({
+    customClass: {
+      confirmButton: 'btn btn-success',
+      cancelButton: 'btn btn-danger'
+    },
+    buttonsStyling: true
+  })
+  
+  swalWithBootstrapButtons.fire({
+    title: '¿Qué quieres hacer primero?',
+    icon: 'question',
+    showCancelButton: true,
+    confirmButtonText: 'Ver simuladores',
+    cancelButtonText: 'Ver tutorial',
+    width: 600,
+    padding: '3em',
+    color: '#716add',
+  }).then((result) => {
+    if (result.isConfirmed) {
+      swalWithBootstrapButtons.fire(
+        window.location.href = "../Newton/newton.html",
+      )
+    } else if (
+      /* Read more about handling dismissals below */
+      result.dismiss === Swal.DismissReason.cancel
+    ) {
+      swalWithBootstrapButtons.fire(
+        window.location.href = "../ComoUsar/ComoUsarNewton.html"
       )
     }
   })
